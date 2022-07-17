34bd27303fcfa893142460a1816191942448ae38
connormitchell4 <69610285+connormitchell4@users.noreply.github.com>
2022-07-17 02:42:00 -0400

    final website without loadingscreen

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ import './style.css';
 import * as THREE from 'three';
 //orbit controls
 
-import { threeToCannon, ShapeType } from 'three-to-cannon';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import Ammo from "ammo.js";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls';
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
-import { PointLight, Raycaster } from 'three';
-import "@babylonjs/core/Debug/debugLayer";
-import "@babylonjs/inspector";
-import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
-//import { Mesh, MeshBasicMaterial, MeshMatcapMaterial, MeshPhongMaterial, MeshStandardMaterial, StaticReadUsage } from 'three';
+import { PointLight, Raycaster, SpotLight } from 'three';
+
+
 
 //scene
 const scene = new THREE.Scene();
@@ -24,20 +19,17 @@ const camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHei
 const camera2 = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.1, 3000);
 camera2.position.set(0,2,4);
 
-//cameraphysics
-
-
-
-
-
 //renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector("#bg")
 })
+
 renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+
+window.scrollTo(0,0);
 window.addEventListener( "resize", function(){
     var width = window.innerWidth;
     var height = window.innerHeight;
@@ -51,15 +43,16 @@ window.addEventListener( "resize", function(){
 //importing custom model from blender with animation
 
 
-var model, environment, navMap;
+var model, environment, navMap, gallery;
 
-const urlModel = new URL("fractal.glb", import.meta.url);
-const urlModelEnvironment = new URL("trainstation_edited.glb", import.meta.url);
-const urlNavMap = new URL("pathway_train.glb", import.meta.url);
+const urlModel = new URL("assets/fractal.glb", import.meta.url);
+const urlModelEnvironment = new URL("assets/trainstation_edited.glb", import.meta.url);
+const urlNavMap = new URL("assets/pathway_train.glb", import.meta.url);
+const urlGallery = new URL("assets/ArtGallery.glb", import.meta.url);
 const assetLoader = new GLTFLoader();
 assetLoader.load(urlModel.href, function(gltf) {
     model = gltf.scene;
-   
+
     scene.add(model);
 
     
@@ -100,14 +93,26 @@ assetLoader.load(urlNavMap.href, function(gltf) {
     navMap.visible = false;
 })
 
+assetLoader.load(urlGallery.href, function(gltf) {
+    gallery = gltf.scene;
+    gallery.rotateY(-Math.PI/2);
+    gallery.scale.multiplyScalar(1);
+    gallery.position.set(7.3, -9.7, 38.6);
+    
+    scene2.add(gallery);
+    gallery.visible = false;
+})
+
 
 //using the navmap function
 const up = new THREE.Vector3(0.0, 1.0, 0.0);
 const down = new THREE.Vector3(0.0, -1.0, 0.0);
-const ENABLE_NAVMESH = true
+const ENABLE_NAVMESH = true;
+const ENABLE_LINKMESH = true;
 
 const positionVector = new THREE.Vector3();
 const collisionVector = new THREE.Vector3();
+
 const raycaster = new THREE.Raycaster(
   positionVector,
   down,
@@ -115,8 +120,9 @@ const raycaster = new THREE.Raycaster(
   10.0
 );
 
+
 let restoreCamPos = new THREE.Vector3();
-const useNavMap = (object) => {
+const useNavMap = (object, navMap, raycaster) => {
     if (!object || !navMap) {
       return;
     }
@@ -156,6 +162,51 @@ const useNavMap = (object) => {
     return true;
   };
 
+//objects and raycasting for opening links in project gallery
+
+const geometry = new THREE.PlaneGeometry( 6, 3 );
+const material = new THREE.MeshBasicMaterial( {side: THREE.DoubleSide} );
+const mosaicPlane = new THREE.Mesh( geometry, material );
+mosaicPlane.position.set(8.5
+    ,-14.71
+    ,47);
+mosaicPlane.rotateX(Math.PI/2);
+
+scene2.add( mosaicPlane );
+
+
+const rocketLeaguePlane = new THREE.Mesh( geometry, material);
+rocketLeaguePlane.position.set(0.27
+    ,-14.71
+    ,55.15);
+rocketLeaguePlane.rotateX(Math.PI/2);
+
+scene2.add( mosaicPlane );
+mosaicPlane.visible = false;
+scene2.add(rocketLeaguePlane);
+rocketLeaguePlane.visible = false;
+
+const linkRaycasting = (object, navPlane, raycaster) => {
+    if (!object || !navPlane) {
+      return;
+    }
+    
+    // Ensure front and back vectors are pointing ±1.0 on X-axis
+    up.normalize();
+    down.normalize();
+    
+    raycaster.set(collisionVector, down);
+    
+    // See if ray intersects the navmesh
+    const hits = raycaster.intersectObject(navPlane, true);
+    
+    if (!hits[0]) {
+        return ENABLE_LINKMESH ? false : true;
+      }
+
+    return true;
+    
+  };
 //animating background based on mouse cursor
 document.addEventListener('mousemove', onDocumentMouseMove)
 
@@ -174,7 +225,7 @@ function onDocumentMouseMove(event) {
 }
 
 //keyboard inputs for camera2
-//const controls = new FirstPersonControls(camera2, renderer.domElement)
+
 const controls = new PointerLockControls(camera2, renderer.domElement);
 let keys = [];
 
@@ -186,34 +237,15 @@ function onKeyUp(event) {
 }
 document.addEventListener('keydown', onKeyDown)
 document.addEventListener("keyup", onKeyUp);
-//controls.lookSpeed = 0;
-//controls.lookSpeed = .5;
-//controls.movementSpeed = 2;
-//controls.noFly = true;
-//controls.lookVertical = false;
+
 
 
 //lighting
 const ambientLight = new THREE.AmbientLight(0xffffff, 2);
-const ambientLight2 = new THREE.AmbientLight(0xffffff, 1);
-const pointLight1 = new THREE.PointLight(0xffffff, 1/16);
+const ambientLight2 = new THREE.AmbientLight(0xffffff, 0);
 
-pointLight1.position.set(-4.5,7,5.2);
-const environmentAmbientLight = new THREE.AmbientLight(0xffffff, 0);
-//const environmentPointLight = new THREE.PointLight(0xffffff, 1);
 scene.add(ambientLight);
 scene2.add(ambientLight2)
-scene2.add(pointLight1)
-
-
-
-const lighthelper = new THREE.PointLightHelper(pointLight1);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene2.add(lighthelper);
-//scene.add(gridHelper);
-
-//orbit controls
-//const controls = new OrbitControls(camera, renderer.domElement);
 
 
 
@@ -230,20 +262,21 @@ var random2 = 0.2 * (2*Math.random()-1)
 var random3 = 0.2 * (2*Math.random()-1) 
 
 //scrolling 
-
-let scrollPos, scrollPercent;
+let scrollPos;
+var scrollPercent = 0;
 window.addEventListener("scroll", (event) => {
     scrollPos = window.scrollY;
     scrollPercent = 100/3*(scrollPos/window.innerHeight)
-    //console.log(scrollPercent)
+    
 })
 
 
 
 
 function animate() {
-    var delta = clock.getDelta();
+    
     const elaspedTime = clock.getElapsedTime();
+
     requestAnimationFrame(animate);
     //background fractal
     
@@ -251,6 +284,7 @@ function animate() {
     model.rotation.y = random1 * elaspedTime/12 ;
     model.rotation.z = random2 * elaspedTime/2;
     model.rotation.x =  random3 * elaspedTime;
+    ambientLight.intensity = Math.max(2-scrollPercent*3/40, 0);
     if(scrollPercent < 100/3) {
         model.visible = true;
         targetX = mouseX * 0.005;
@@ -259,7 +293,7 @@ function animate() {
         
         camera.rotation.x = -scrollPercent/30;
         ambientLight.color.setHSL(1/2*Math.sin(elaspedTime/32+(random1*100))+1/2, 1/2, 1/2);
-        
+       
         ambientLight2.intensity = 0;
         renderer.render(scene, camera);
     
@@ -267,33 +301,62 @@ function animate() {
         renderer.render(scene2, camera2)
     }
 }
-    if (scrollPercent > 75) {
+    if (scrollPercent > 79) {
         
         if(environment) {environment.visible = true;
+            gallery.visible = true;
 
         }
     } else {
-        if(environment) {environment.visible =false;}
-    }
+        if(environment) {environment.visible =false; 
+            gallery.visible = false;
+    }}
     document.getElementById("home").style.opacity = Math.max(0, 1-(scrollPercent/15));
-    document.getElementById("background").style.opacity = Math.max(Math.min(1, (scrollPercent-10)/23), 0);
-    document.getElementById("projects").style.opacity = Math.max(Math.min(1, (scrollPercent-80)/20), 0);
-    ambientLight2.intensity = Math.max(Math.min((scrollPercent-80)/20, 1/8), 0);
-    ambientLight.intensity = Math.max(2-scrollPercent*3/50, 0);
+    if(scrollPercent <33){
+        document.getElementById("background").style.opacity = Math.max((scrollPercent-10)/23, 0);}
+    else {
+        document.getElementById("background").style.opacity = Math.max(1-(scrollPercent-33)/10, 0);
+    }
+    if(scrollPercent < 65) {
+        document.getElementById("projects").style.opacity = Math.max((scrollPercent-50)/15, 0);}
+    else {
+        document.getElementById("projects").style.opacity = Math.max(1-(scrollPercent-65)/15, 0);
+    }
+    ambientLight2.intensity = Math.max(Math.min((scrollPercent-80)/(20*16), 1/16), 0);
+    
     
     if (scrollPercent > 99) {
         //controls.connect()
         controls.lock()
-        if (!useNavMap(camera2)) {
+       
+        if (!useNavMap(camera2, navMap, raycaster)) {
            camera2.position.copy(restoreCamPos);
            
           }
-            if(keys['w']){
-            controls.moveForward(Math.min(x, 0.05));
-            x+=0.001;
-            } else {
-                x = 0;
+        if(linkRaycasting(camera2, mosaicPlane, raycaster)){
+            document.getElementById("contact").style.opacity = 1;
+            if(keys['f']){
+                window.open("https://youtu.be/AWq0PV-1nk8");
+                keys['f'] = false;
             }
+        } else if(linkRaycasting(camera2, rocketLeaguePlane, raycaster)){
+            document.getElementById("contact").style.opacity = 1;
+            if(keys['f']){
+                window.open("https://www.youtube.com/watch?v=FxbO10fc_nA&ab_channel=Connor");
+                keys['f'] = false;
+            }
+        } else {
+            document.getElementById("contact").style.opacity = 0;
+        };
+        
+           
+            if(keys['w']){
+                controls.moveForward(Math.min(x, 0.06));
+                x+=0.001;
+                } 
+                else {
+                    x = 0;
+                }
             if(keys['s']){
             controls.moveForward(-Math.min(y,.05));
             y+=0.001;
@@ -312,8 +375,8 @@ function animate() {
             } else {
                 w = 0;
             }
+            
 
-        //controls.handleResize();
     }
     else {
         controls.unlock();
